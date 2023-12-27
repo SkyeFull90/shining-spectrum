44924e79b546520deb075b3aa93b1d69f94e0704
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-27 14:46:13 -0700

    Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import solidJs from "@astrojs/solid-js";
-import react from "@astrojs/react";
 
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), solidJs(), react()],
+  integrations: [tailwind(), react()],
   adapter: vercel()
 });
