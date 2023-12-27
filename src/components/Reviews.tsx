ff45b98a4071c756040cfaea03973d1de1c068d9
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-27 13:00:10 -0700

    Refactor Reviews component and add empty div in dashboard page

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,71 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { supabase } from '../lib/supabase'; // adjust the path as needed
+import React, { useState, useEffect } from 'react';
+import { supabase } from '../lib/supabase';
 
-type FormData = {
-    name: string;
-    review: string;
-};
-
-type ReviewData = {
-    id: string;
-    name: string;
-    review: string;
-};
+interface Review {
+  id: number;
+  name: string;
+  review: string;
+}
 
 const Reviews: React.FC = () => {
-    const { register, handleSubmit, reset } = useForm<FormData>();
-    const [reviews, setReviews] = useState<ReviewData[]>([]);
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [newReview, setNewReview] = useState('');
 
-    useEffect(() => {
-        fetchReviews();
-    }, []);
+  useEffect(() => {
+    fetchReviews();
+  }, []);
 
-    const fetchReviews = async () => {
-        const { data, error } = await supabase.from('reviews').select('*');
-        if (error) console.error('Error fetching reviews:', error);
-        else setReviews(data || []);
-    };
+  const fetchReviews = async () => {
+    const { data, error } = await supabase.from('reviews').select('*');
+    if (error) console.error('Error fetching reviews', error);
+    else setReviews(data || []);
+  };
 
-    const onSubmit = async (data: FormData) => {
-        const { name, review } = data;
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { error } = await supabase.from('reviews').insert([{ review: newReview }]);
+    if (error) console.error('Error submitting new review', error);
+    else {
+      setNewReview('');
+      fetchReviews();
+    }
+  };
 
-        // Send data to your Supabase table
-        const { error } = await supabase
-            .from('reviews')
-            .insert([{ name, review }]);
-
-        if (error) {
-            console.error('Error submitting review:', error);
-        } else {
-            reset(); // Reset form after successful submission
-            fetchReviews(); // Fetch the latest reviews
-        }
-    };
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label>
-                    Name:
-                    <input {...register('name', { required: true })} />
-                </label>
-                <label>
-                    Review:
-                    <textarea {...register('review', { required: true })} />
-                </label>
-                <button type="submit">Submit Review</button>
-            </form>
-            <ul>
-                {reviews.map((review) => (
-                    <li key={review.id}>
-                        <p>{review.name}</p>
-                        <p>{review.review}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Reviews</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newReview}
+          onChange={(e) => setNewReview(e.target.value)}
+          required
+        />
+        <button type="submit">Submit Review</button>
+      </form>
+      <ul>
+        {reviews.map((review) => (
+          <li key={review.id}>
+            <p>{review.name}</p>
+            <p>{review.review}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Reviews;
