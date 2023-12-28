9a7b8a2e4ec844958c3eaf75d8e8540614bbd250
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-27 17:56:48 -0700

    Update dependencies and add Solid.js integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), solidJs()],
   adapter: vercel()
 });
