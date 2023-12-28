7f94c7d3f4e248a7ba59803eaf885ddfe78f61da
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-28 14:06:35 -0700

    Refactor API route and update dashboard background image

--- src/pages/api/reviews.ts
+++ src/pages/api/reviews.ts
@@ -4,15 +4,11 @@ import { supabase } from "../../lib/supabase";
 export const GET: APIRoute = async () => {
     const { data, error } = await supabase
         .from("reviews")
-        .select("*")
-        .limit(10);
+        .select("*");
 
     if (error) {
         return new Response(error.message, { status: 500 });
     }
 
-    return new Response(JSON.stringify(data), {
-        headers: { "content-type": "application/json" },
-        status: 200,
-    });
+    return new Response(JSON.stringify(data));
 }
